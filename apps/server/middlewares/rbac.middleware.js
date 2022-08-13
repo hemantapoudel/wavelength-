e66152bfff494a24b7e8839307d5be994cba85df
Hemanta Poudel <hemantapoudel14@gmail.com>
2022-08-13 21:00:23 +0545

    RBAC middleware created

---
+++ apps/server/middlewares/rbac.middleware.js
@@ -0,0 +1,54 @@
+const User = require("../models/user.model")
+const isSuperadmin = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && user.role === "super_admin"){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
+
+const isAdmin = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && user.role === "admin"){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
+
+const isModerator = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && user.role === "moderator"){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
+
+const isMentor = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && user.role === "mentor"){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
+
+const isStudent = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && user.role === "student"){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
+
+const isSuperadminOrAdmin = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && (user.role === "super_admin" || user.role === "admin")){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
