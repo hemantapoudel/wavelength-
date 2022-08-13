ed7e191e646a4e2ffc3ca1564c2810b46982ca59
Hemanta Poudel <hemantapoudel14@gmail.com>
2022-08-13 21:09:20 +0545

    rbac updated

--- apps/server/middlewares/rbac.middleware.js
+++ apps/server/middlewares/rbac.middleware.js
@@ -52,3 +52,29 @@ const isSuperadminOrAdmin = (req,res,next) => {
         next({status:403, msg:"Unauthorized Access"})
     }
 }
+
+const isVerified = (req,res,next) => {
+    let user =req.auth_user;
+    if(user && user.is_verified === true ){
+        next()
+    } else{
+        next({status:403, msg:"Unauthorized Access"})
+    }
+}
+
+const selfUpdate = async (req, res, next) => {
+    try {
+        let user = await User.findById(req.params.id);
+        if(user.id === req.auth_user.id){
+            next()
+        } else{
+            next({status:403,msg:"Unauthorized Access"})
+        }
+    } 
+    catch(error){
+        next(error)
+    }
+}
+
+
+module.exports = {isSuperadmin,isAdmin,isModerator,isMentor,isStudent,isSuperadminOrAdmin, isVerified, selfUpdate}
