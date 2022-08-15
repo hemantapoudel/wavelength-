2fdcd567fd1909f42688a600c153abc67f81b4e7
Gaurav Pandey <gauravpandey2058@gmail.com>
2022-08-15 18:47:17 +0545

    feat(store:auth): onboard

--- packages/store/useAuth.ts
+++ packages/store/useAuth.ts
@@ -4,15 +4,18 @@ interface AuthState {
         name: string
     },
     isLoading: boolean,
+    showOnboard: boolean,
     setUser: (user: AuthState['user']) => void,
     logOut: () => void,
+    setOnboardFalse: () => void
 }
 
 
 export const useAuth = create<AuthState>((set) => ({
     user: null,
+    showOnboard: true,
     isLoading: false,
     setUser: (user) => set((state) => ({ user })),
     logOut: () => set((state) => ({ user: null })),
-
+    setOnboardFalse: () => set({ showOnboard: false })
 }))
